1f9dc5c79c07841813b51146cd1ac722fa477213
jdc-cunningham <jcun4128@gmail.com>
2019-08-06 20:32:27 -0500

    progress save, project slider - demo data and structure, basic layout in progress

---
+++ portfolio/src/assets/demo-data/userProjects.js
@@ -0,0 +1,24 @@
+const USERPROJECTS = {
+  'du3dk' : {
+    name: "Project name",
+    dateAdded: "08/06/2019",
+    dateCompleted: "05/06/2019",
+    description: "demo project description",
+    link: "example.com/project",
+    categories: [
+      'professional'
+    ],
+    techStack: [
+      'WordPress',
+      'SASS',
+      'jQuery'
+    ],
+    photos: [
+      'pixabay_1.jpg',
+      'pixabay_2.jpg',
+      'pixabay_3.jpg'
+    ]
+  }
+}
+
+export default USERPROJECTS;
--- portfolio/src/components/content-slider/ContentSlider.js
+++ portfolio/src/components/content-slider/ContentSlider.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContentSlider.scss';
+import USERPROJECTS from './../../assets/demo-data/userProjects'; // will be replaced by API
 
 const ContentSlider = () => {
+  const imagePrefix = 'http://192.168.1.105:3000/src/assets/demo-data/project-images/',
+        initProjects = {
+          activeProjectId: 'du3dk',
+          userProjects: USERPROJECTS
+        },
+        [projects, setProject] = useState( initProjects );
+
+  // this is ugly
+  const activeProjectSliderImage = typeof projects.userProjects[projects.activeProjectId]['photos'][0] !== undefined ?
+          projects.userProjects[projects.activeProjectId]['photos'][0] : '';
+
+  console.log( activeProjectSliderImage );
+
   return (
-    <div className="ContentSlider"></div>
+    <div className="ContentSlider">
+      <div className="ContentSlider__photo-slider" style={ {backgroundImage: "url('/demo-images/" + activeProjectSliderImage + "')"} }>
+        <div className="photo-slider__banner"></div>
+      </div>
+      <div className="ContentSlider__project-slider"></div>
+      <div className="ContentSlider__project-details"></div>
+    </div>
   )
 }
 
